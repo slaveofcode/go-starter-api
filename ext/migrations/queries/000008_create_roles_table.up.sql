f470e33a0194ab70b4677f60f3432c4b7a631007
Aditya Kresna Permana <zeandcode@gmail.com>
2019-10-22 22:34:56 +0700

    Added roles table migration

---
+++ ext/migrations/queries/000008_create_roles_table.up.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS "Roles" (
+   id serial PRIMARY KEY,
+   name VARCHAR(30) NOT NULL,
+   scopes VARCHAR[] NOT NULL,
+   "CreatedAt" TIMESTAMP WITH TIME ZONE NOT NULL,
+   "UpdatedAt" TIMESTAMP WITH TIME ZONE NOT NULL,
+   "DeletedAt" TIMESTAMP WITH TIME ZONE NULL
+);
+
+CREATE INDEX "Roles_scopes_idx" ON "Roles" ("scopes");
+
+CREATE INDEX "Roles_CreatedAt_idx" ON "Roles" ("CreatedAt");
+CREATE INDEX "Roles_UpdatedAt_idx" ON "Roles" ("UpdatedAt");
+CREATE INDEX "Roles_DeletedAt_idx" ON "Roles" ("DeletedAt");
