b9c8f4040fb71a14cedc1f7e68875001c9767a65
Aditya Kresna Permana <zeandcode@gmail.com>
2019-11-18 23:36:34 +0700

    Added role on invitation member

--- ext/migrations/queries/000010_create_team_member_invitations_table.up.sql
+++ ext/migrations/queries/000010_create_team_member_invitations_table.up.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS "TeamMemberInvitations" (
    id serial PRIMARY KEY,
    "teamId" INTEGER NOT NULL REFERENCES "Teams"(id),
    email VARCHAR(60) NOT NULL,
+   "roleId" INTEGER NOT NULL REFERENCES "Roles"(id),
    "invitationKey" VARCHAR(32) NOT NULL,
    "CreatedAt" TIMESTAMP WITH TIME ZONE NOT NULL,
    "UpdatedAt" TIMESTAMP WITH TIME ZONE NOT NULL,
@@ -10,6 +11,7 @@ CREATE TABLE IF NOT EXISTS "TeamMemberInvitations" (
 
 CREATE INDEX "TeamMemberInvitations_teamId_idx" ON "TeamMemberInvitations" ("teamId");
 CREATE INDEX "TeamMemberInvitations_email_idx" ON "TeamMemberInvitations" ("email");
+CREATE INDEX "TeamMemberInvitations_roleId_idx" ON "TeamMemberInvitations" ("roleId");
 CREATE INDEX "TeamMemberInvitations_invitationKey_idx" ON "TeamMemberInvitations" ("invitationKey");
 
 CREATE INDEX "TeamMemberInvitations_CreatedAt_idx" ON "TeamMemberInvitations" ("CreatedAt");
