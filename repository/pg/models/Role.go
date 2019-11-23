668529d739b46c80ffe9ef38e5f30096cfcccdab
Aditya Kresna Permana <zeandcode@gmail.com>
2019-11-23 13:04:45 +0700

    Fix role string array column type

--- repository/pg/models/Role.go
+++ repository/pg/models/Role.go
@@ -1,17 +1,18 @@
 package models
 
 import (
+	"github.com/lib/pq"
 	"time"
 )
 
 // Role model gorm
 type Role struct {
-	ID        uint       `gorm:"primary_key"`
-	Name      string     `gorm:"column:name" sql:"index"`
-	Scopes    []string   `gorm:"column:scopes" sql:"index"`
-	CreatedAt time.Time  `gorm:"column:CreatedAt" sql:"index"`
-	UpdatedAt time.Time  `gorm:"column:UpdatedAt" sql:"index"`
-	DeletedAt *time.Time `gorm:"column:DeletedAt" sql:"index"` // *time.Time to support nil on gorm model
+	ID        uint           `gorm:"primary_key"`
+	Name      string         `gorm:"column:name" sql:"index"`
+	Scopes    pq.StringArray `gorm:"column:scopes;type:varchar[]" sql:"index"`
+	CreatedAt time.Time      `gorm:"column:CreatedAt" sql:"index"`
+	UpdatedAt time.Time      `gorm:"column:UpdatedAt" sql:"index"`
+	DeletedAt *time.Time     `gorm:"column:DeletedAt" sql:"index"` // *time.Time to support nil on gorm model
 }
 
 // TableName describe name of the table
