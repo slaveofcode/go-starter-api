950f55cea3fce0f92c2646deec1bee31de9f349c
Aditya Kresna Permana <zeandcode@gmail.com>
2019-11-23 23:46:18 +0700

    Added more rows

--- ext/migrations/queries/000018_seed_roles_data.up.sql
+++ ext/migrations/queries/000018_seed_roles_data.up.sql
@@ -1,2 +1,4 @@
 INSERT INTO "Roles" (name, scopes, "CreatedAt", "UpdatedAt")
-VALUES('Owner', '{"team:read","team:write","invoice:read","invoice:write","subscription:read","subscription:write","apikey:read","apikey:write"}', now(), now());
+VALUES('Owner', '{"team:read","team:write","invoice:read","invoice:write","subscription:read","subscription:write","apikey:read","apikey:write"}', now(), now()),
+VALUES('Manager', '{"team:read","team:write","invoice:read","subscription:read","apikey:read","apikey:write"}', now(), now()),
+VALUES('Staff', '{"team:read","subscription:read","apikey:read"}', now(), now());
