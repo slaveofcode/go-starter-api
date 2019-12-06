027069c5e8130b00203c261f6a60f45dfabce363
Aditya Kresna Permana <zeandcode@gmail.com>
2019-12-06 22:28:32 +0700

    Fix insert query

--- ext/migrations/queries/000018_seed_roles_data.up.sql
+++ ext/migrations/queries/000018_seed_roles_data.up.sql
@@ -1,4 +1,4 @@
 INSERT INTO "Roles" (name, scopes, "CreatedAt", "UpdatedAt")
 VALUES('Owner', '{"team:read","team:write","invoice:read","invoice:write","subscription:read","subscription:write","apikey:read","apikey:write"}', now(), now()),
-VALUES('Manager', '{"team:read","team:write","invoice:read","subscription:read","apikey:read","apikey:write"}', now(), now()),
-VALUES('Staff', '{"team:read","subscription:read","apikey:read"}', now(), now());
+('Manager', '{"team:read","team:write","invoice:read","subscription:read","apikey:read","apikey:write"}', now(), now()),
+('Staff', '{"team:read","subscription:read","apikey:read"}', now(), now());
